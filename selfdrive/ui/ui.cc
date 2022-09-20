24872dc3d0049c7233784a4cbe489e93b6a75801
chanhojung <jungch@naver.com>
2022-09-20 18:49:26 +0900

    Revert "UI: draw path from planner #25844"
    This reverts commit 85d7f3d572e86fb4aa7bdfd14940e15efea69f9c.

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -54,28 +54,18 @@ static void update_leads(UIState *s, const cereal::RadarState::Reader &radar_sta
   }
 }
 
-static void update_line_data(const UIState *s, const ::capnp::List<float, ::capnp::Kind::PRIMITIVE>::Reader &line_x,
-                             const ::capnp::List<float, ::capnp::Kind::PRIMITIVE>::Reader &line_y,
-                             const ::capnp::List<float, ::capnp::Kind::PRIMITIVE>::Reader &line_z,
-                             float y_off, float z_off, QPolygonF *pvd, int max_idx, bool allow_invert=true) {
-
-  QPolygonF left_points, right_points;
-  left_points.reserve(max_idx + 1);
-  right_points.reserve(max_idx + 1);
+static void update_line_data(const UIState *s, const cereal::ModelDataV2::XYZTData::Reader &line,
+                             float y_off, float z_off, line_vertices_data *pvd, int max_idx) {
+  const auto line_x = line.getX(), line_y = line.getY(), line_z = line.getZ();
+  vertex_data *v = &pvd->v[0];
   for (int i = 0; i <= max_idx; i++) {
-    QPointF left, right;
-    bool l = calib_frame_to_full_frame(s, line_x[i], line_y[i] - y_off, line_z[i] + z_off, &left);
-    bool r = calib_frame_to_full_frame(s, line_x[i], line_y[i] + y_off, line_z[i] + z_off, &right);
-    if (l && r) {
-      // For wider lines the drawn polygon will "invert" when going over a hill and cause artifacts
-      if (!allow_invert && left_points.size() && left.y() > left_points.back().y()) {
-        continue;
-      }
-      left_points.push_back(left);
-      right_points.push_front(right);
-    }
+    v += calib_frame_to_full_frame(s, line_x[i], line_y[i] - y_off, line_z[i] + z_off, v);
   }
-  *pvd = left_points + right_points;
+  for (int i = max_idx; i >= 0; i--) {
+    v += calib_frame_to_full_frame(s, line_x[i], line_y[i] + y_off, line_z[i] + z_off, v);
+  }
+  pvd->cnt = v - pvd->v;
+  assert(pvd->cnt <= std::size(pvd->v));
 }
 
 static void update_blindspot_data(const UIState *s, const cereal::ModelDataV2::XYZTData::Reader &line,
@@ -116,7 +106,7 @@ static void update_model(UIState *s, const cereal::ModelDataV2::Reader &model) {
   int max_idx = get_path_length_idx(lane_lines[0], max_distance);
   for (int i = 0; i < std::size(scene.lane_line_vertices); i++) {
     scene.lane_line_probs[i] = lane_line_probs[i];
-    update_line_data(s, lane_lines[i].getX(), lane_lines[i].getY(), lane_lines[i].getZ(), 0.05 * scene.lane_line_probs[i], 0, &scene.lane_line_vertices[i], max_idx);
+    update_line_data(s, lane_lines[i], 0.05 * scene.lane_line_probs[i], 0, &scene.lane_line_vertices[i], max_idx);
   }
 
   // update road edges
@@ -124,8 +114,17 @@ static void update_model(UIState *s, const cereal::ModelDataV2::Reader &model) {
   const auto road_edge_stds = model.getRoadEdgeStds();
   for (int i = 0; i < std::size(scene.road_edge_vertices); i++) {
     scene.road_edge_stds[i] = road_edge_stds[i];
-    update_line_data(s, road_edges[i].getX(),road_edges[i].getY() , road_edges[i].getZ(), 0.2, 0, &scene.road_edge_vertices[i], max_idx);
+    update_line_data(s, road_edges[i], 0.2, 0, &scene.road_edge_vertices[i], max_idx);
   }
+
+  // update path
+  auto lead_one = (*s->sm)["radarState"].getRadarState().getLeadOne();
+  if (lead_one.getStatus()) {
+    const float lead_d = lead_one.getDRel() * 2.;
+    max_distance = std::clamp((float)(lead_d - fmin(lead_d * 0.35, 10.)), 0.0f, max_distance);
+  }
+  max_idx = get_path_length_idx(model_position, max_distance);
+  update_line_data(s, model_position, 1.0, 1.22, &scene.track_vertices, max_idx);
 
    // update blindspot line
   scene.lane_blindspot_probs[0] = lane_line_probs[1];
@@ -140,18 +139,6 @@ static void update_model(UIState *s, const cereal::ModelDataV2::Reader &model) {
     if (stop_line.getProb() > .5) {
       update_stop_line_data(s, stop_line, .5, 2, 1.22, &scene.stop_line_vertices);
     }
-  }
-}
-
-static void update_plan(UIState *s) {
-  // update path
-  const auto plan_y = (*s->sm)["lateralPlan"].getLateralPlan().getYs();
-  const auto plan_x = (*s->sm)["longitudinalPlan"].getLongitudinalPlan().getXs();
-  const auto plan_z = (*s->sm)["longitudinalPlan"].getLongitudinalPlan().getZs();
-
-  if (plan_y.size() == CONTROL_N && plan_x.size() == CONTROL_N && plan_z.size() == CONTROL_N) {
-    UIScene &scene = s->scene;
-    update_line_data(s, plan_x, plan_y, plan_z , 1.0, 1.22, &scene.track_vertices, CONTROL_N - 1, false);
   }
 }
 
@@ -249,9 +236,6 @@ static void update_state(UIState *s) {
   if (sm.updated("modelV2") && s->vg) {
     update_model(s, sm["modelV2"].getModelV2());
   }
-  if (sm.updated("longitudinalPlan")) {
-    update_plan(s);
-  }  
   if (sm.updated("radarState") && sm.rcv_frame("modelV2") >= s->scene.started_frame) {
     update_leads(s, sm["radarState"].getRadarState(), sm["modelV2"].getModelV2().getPosition());
   }
@@ -578,7 +562,7 @@ QUIState::QUIState(QObject *parent) : QObject(parent) {
   ui_state.sm = std::make_unique<SubMaster, const std::initializer_list<const char *>>({
     "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState", "roadCameraState",
     "pandaStates", "carParams", "driverMonitoringState", "sensorEvents", "carState", "liveLocationKalman",
-    "ubloxGnss", "gpsLocationExternal", "liveParameters", "liveNaviData", "liveENaviData", "liveMapData", "lateralPlan", "longitudinalPlan",
+    "ubloxGnss", "gpsLocationExternal", "liveParameters", "lateralPlan", "liveNaviData",  "liveENaviData","liveMapData", "longitudinalPlan",
   });
 
   Params params;
